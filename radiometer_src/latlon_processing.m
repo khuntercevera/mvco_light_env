b7bb0ff63a0d4a9057ec68118299a810dd3117ec
Kristen Hunter-Cevera <kristenhunter-cevera@dyn-128-128-10-104.whoi.edu>
2017-07-31 16:11:31 -0400

    added piece to find good data, will need to record lat/lon manual entries...

--- radiometer_src/latlon_processing.m
+++ radiometer_src/latlon_processing.m
@@ -9,6 +9,28 @@ foldernames = {d(isub).name}'; %names of folders
 temp=regexp(foldernames,'\d{1,2}\w{3,4}20\d{2}'); %find all folders with this naming scheme
 datafolders = foldernames(cellfun('isempty',temp)==0);
 
+%To avoid chasing down data for empty casts, identify the folders that have good data in them:
+good_data=[];
+bad_data=[];
+for foldernum=1:length(datafolders)
+    
+    eval(['load ' sourcepath datafolders{foldernum} '/mat_outfiles/data_' datafolders{foldernum} '.mat'])
+    eval(['tempdata=data_' datafolders{foldernum} ';'])
+    
+    if any(cellfun(@(x) any(x==0),{tempdata(:).emptyflag})==1) % meaning at least one good cast
+        disp('Found good casts...')
+        good_data=[good_data; foldernum];
+    else
+        disp('Only empty files for this folder...')
+        bad_data=[bad_data; foldernum];
+    end
+end
+
+%% Okay, so the plan is...
+
+%loop through good folders and see if lat/lon was recorded. If not, see if
+%can't backtrack this from MVCO nutrient logs that have a station number in
+%them...
 
 %to place lat/lon and station that goes along with each cast, use info in nutrient file:
 load /Users/kristenhunter-cevera/Documents/MATLAB/MVCO_Syn_analysis/MVCO_environmental_data/nut_data_repsB.mat
@@ -29,18 +51,12 @@ for q=1:length(datafolders)
     
 end
 
-
-%excellent - the ones that don't match are empty anyways!
-good_data=[2:3 5:10 12 14:16 18:20];
-bad_data=setxor(good_data,1:20);
-
-
 %% Okay, now walk through each day of data - see if lat/lons match to MVCO event data
 %If so, record station number
 %If not, try to piece that back together!
 
 
-for foldernum=[7:10 12 14:16 18:20] %[2:3 5:10 12 14:16 18:20]; %files that have data in them!
+for foldernum=good_data' %[7:10 12 14:16 18:20] %[2:3 5:10 12 14:16 18:20]; %files that have data in them!
     
     matsource=fullfile(sourcepath,datafolders{foldernum},'/mat_outfiles/');
     
@@ -70,20 +86,26 @@ for foldernum=[7:10 12 14:16 18:20] %[2:3 5:10 12 14:16 18:20]; %files that have
             
             if mvco_latlon(io,3) ~= mvco_latlon(ia,3) %if stations do not match, then have a problem...
                 location(q).station=[];
+                disp('Station is mismatched between lat and lon')
             else
                 location(q).station=mvco_latlon(io,3);
             end
                 
+                eval(['location_' datafolders{foldernum} '=location;'])
+                eval(['save ' matsource 'location_' datafolders{foldernum} ' location_' datafolders{foldernum}])
+                clear location
+    
         else %need to look at mvco records...
             disp(['Need to look at the MVCO records for this one! ' tempdata(q).file '  ' datafolders{foldernum} '  ' num2str(foldernum)])   
         end
-        
+         
     end %files within tempdata
     
-    eval(['location_' datafolders{foldernum} '=location;'])
-    eval(['save ' matsource 'location_' datafolders{foldernum} ' location_' datafolders{foldernum}])
-    clear location
+%     eval(['location_' datafolders{foldernum} '=location;'])
+%     eval(['save ' matsource 'location_' datafolders{foldernum} ' location_' datafolders{foldernum}])
+%     clear location
     
+    pause
 end %foldernum
 
 
