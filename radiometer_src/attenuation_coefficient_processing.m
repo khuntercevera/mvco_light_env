b720f8476ac866846a890137031fcca3e3c60d81
Kristen Hunter-Cevera <kristenhunter-cevera@v-128-128-10-203.whoi.edu>
2017-09-11 16:15:49 -0400

    fixed bug where stats record was only being recorded for one part of a piecewise linear fit

--- radiometer_src/attenuation_coefficient_processing.m
+++ radiometer_src/attenuation_coefficient_processing.m
@@ -29,7 +29,7 @@ load(fullfile(sourcepath,'good_data_folders.mat'))
 
 %%
 %can just manually enter in a foldernum for now .... put into a for loop later....
-foldernum=good_data(15);
+foldernum=19; %good_data(15);
 
 %find the data!
 matsource=fullfile(processed_path,datafolders{foldernum},'/mat_outfiles/');
@@ -95,58 +95,6 @@ for filenum=1:numfiles
         
         %for easier handling:
         depth=tempdata(filenum).depth;
-        edl_ind=tempdata(filenum).edl_ind;
-        edl_PAR=tempdata(filenum).edl_PAR;
-        
-        dz=smooth2(diff(depth),15); %change in depth, smoothed to reduce noise
-        dc=find(dz > 0.025); %dc for downcast
-        
-        jj=find(depth(dc) > 2 & depth(dc) < max(depth)-1.5); %exclude top and bottom measurements
-        %jj=find(depth(dc) > 2 & depth(dc) < max(depth)-0.5); %special cases - closer to bottom, for short casts
-        %jj=find(depth(dc) > 2 & depth(dc) < max(depth)-20); %special case for deep casts, change in K with deeper depths
-        
-        %matching indexes for PAR:
-        [~,impr,ipar]=intersect(dc(jj),edl_ind);
-        impr=dc(jj(impr)); %ipar already indexes into edl_PAR...
-        
-        %         impr=impr(32:end);
-        %         ipar=ipar(32:end);
-        
-        %now, calculate k:
-        [K,~,~,~,STATS] = regress(log(edl_PAR(ipar)),[ones(size(depth(impr))) depth(impr)]);
-        
-        %for a split k:
-        %          d1=find(depth(impr) < 16);
-        %          d2=find(depth(impr) > 17);
-        %         [K1,~,~,~,STATS1] = regress(log(edl_PAR(ipar(d1))),[ones(size(depth(impr(d1)))) depth(impr(d1))]);
-        %         [K2,~,~,~,STATS2] = regress(log(edl_PAR(ipar(d2))),[ones(size(depth(impr(d2)))) depth(impr(d2))]);
-        
-        if STATS(1) < 0.9
-            disp('Hmmm...maybe not such a great regression fit to find k?')
-        end
-        
-        %plot those results!
-        figure(filenum), set(gcf,'position',[33         468        1218         510])
-        
-        subplot(1,2,1,'replace')
-        [ax h1 h2]=plotyy(tempdata(filenum).mprtime,depth,tempdata(filenum).mprtime(edl_ind),edl_PAR);
-        hold(ax(1)); hold(ax(2));
-        plot(ax(1),tempdata(filenum).mprtime(impr),depth(impr),'.','markersize',12)
-        plot(ax(2),tempdata(filenum).mprtime(impr),edl_PAR(ipar),'.','markersize',12)
-        set(ax(1),'YDir','reverse','xgrid','on','ygrid','on')
-        ylabel(ax(1),'Depth')
-        ylabel(ax(2),'PAR')
-        datetick(ax(2))
-        datetick(ax(1))
-        
-        subplot(1,2,2,'replace')
-        plot(log(edl_PAR),depth(edl_ind),'.'), hold on
-        plot(log(edl_PAR(ipar)),depth(impr),'.')
-        plot(K(1)+K(2)*depth(impr),depth(impr),'-')
-        set(gca,'YDir','reverse')
-        title(['File: ' num2str(filenum) ' ; ' filename])
-        xl=get(gca,'xlim');  yl=get(gca,'ylim');
-        text(0.80*diff(xl)+xl(1),0.95*diff(yl)+yl(1),{['K: ' num2str(K(2))];['R2: ' num2str(STATS(1))] })
         
         if max(depth) < 7
             disp('too short of a cast')
@@ -154,37 +102,175 @@ for filenum=1:numfiles
             K_PAR(filenum).NOTES='depth too short for reliable cast, excluding...';
             K_PAR(filenum).flag=2;
             
-        else
+        else %can proceed with processing the cast:
             
-            K_PAR(filenum).file=filename;
-            K_PAR(filenum).stats=STATS;
-            K_PAR(filenum).K=K;
-            K_PAR(filenum).depth_index=impr;
-            K_PAR(filenum).par_index=ipar;
-            K_PAR(filenum).NOTES=notes;
-            K_PAR(filenum).flag=0;
+            edl_ind=tempdata(filenum).edl_ind;
+            edl_PAR=tempdata(filenum).edl_PAR;
             
-            %split cast:
-            %             K_PAR(filenum).file=filename;
-            %             K_PAR(filenum).stats=STATS1;
-            %             K_PAR(filenum).K1=K1;
-            %             K_PAR(filenum).depth_index1=impr(d1);
-            %             K_PAR(filenum).par_index1=ipar(d1);
-            %             K_PAR(filenum).stats=STATS2;
-            %             K_PAR(filenum).K2=K2;
-            %             K_PAR(filenum).depth_index2=impr(d2);
-            %             K_PAR(filenum).par_index2=ipar(d2);
-            %             K_PAR(filenum).NOTES='split cast from 0-15 and 16-max';
-            %             K_PAR(filenum).flag=3;
+            dz=smooth2(diff(depth),15); %change in depth, smoothed to reduce noise
+            dc=find(dz > 0.025); %dc for downcast
+            
+            jj=find(depth(dc) > 2 & depth(dc) < max(depth)-1.5); %exclude top and bottom measurements
+            %jj=find(depth(dc) > 2 & depth(dc) < max(depth)-0.5); %special cases - closer to bottom, for short casts
+            %jj=find(depth(dc) > 2 & depth(dc) < max(depth)-20); %special case for deep casts, change in K with deeper depths
+            
+            %matching indexes for PAR:
+            [~,impr,ipar]=intersect(dc(jj),edl_ind);
+            impr=dc(jj(impr)); %ipar already indexes into edl_PAR...
+            
+            %Plot first and give option to do a split cast:
+            figure(filenum), set(gcf,'position',[33         468        1218         510])
+            subplot(1,2,1,'replace')
+            [ax h1 h2]=plotyy(tempdata(filenum).mprtime,depth,tempdata(filenum).mprtime(edl_ind),edl_PAR);
+            hold(ax(1)); hold(ax(2));
+            plot(ax(1),tempdata(filenum).mprtime(impr),depth(impr),'.','markersize',12)
+            plot(ax(2),tempdata(filenum).mprtime(impr),edl_PAR(ipar),'.','markersize',12)
+            set(ax(1),'YDir','reverse','xgrid','on','ygrid','on')
+            ylabel(ax(1),'Depth')
+            ylabel(ax(2),'PAR')
+            datetick(ax(2))
+            datetick(ax(1))
+            
+            subplot(1,2,2,'replace')
+            plot(log(edl_PAR),depth(edl_ind),'.'), hold on
+            plot(log(edl_PAR(ipar)),depth(impr),'.')
+            
+            set(gca,'YDir','reverse')
+            title(['File: ' num2str(filenum) ' ; ' filename])
+            xl=get(gca,'xlim');  yl=get(gca,'ylim');
+            
+            %Okay, now ask if need to fit linear or piecewise:
+            [ch]=input('See if data supports a linear or piecewise linear fit: \n Enter "linear" if one line is good or "piecewise" if need two lines or "other" if need to do something else \n');
+            
+            switch ch
+                
+                case 'linear'
+                    
+                    %now, calculate k:
+                    [K,~,~,~,STATS] = regress(log(edl_PAR(ipar)),[ones(size(depth(impr))) depth(impr)]);
+                    
+                    plot(K(1)+K(2)*depth(impr),depth(impr),'-')
+                    text(0.80*diff(xl)+xl(1),0.95*diff(yl)+yl(1),{['K: ' num2str(K(2))];['R2: ' num2str(STATS(1))] })
+                    
+                    K_PAR(filenum).file=filename;
+                    K_PAR(filenum).stats=STATS;
+                    K_PAR(filenum).K=K;
+                    K_PAR(filenum).depth_index=impr;
+                    K_PAR(filenum).par_index=ipar;
+                    K_PAR(filenum).NOTES='';
+                    K_PAR(filenum).flag=0;
+                    
+                    keyboard
+                    
+                case 'piecewise'
+                    
+                    [dbreak]=input('enter a 2 element vector of depths to where you want to split the line \n');
+                    
+                    %for a split k:
+                    d1=find(depth(impr) < dbreak(1));
+                    d2=find(depth(impr) > dbreak(2));
+                    [K1,~,~,~,STATS1] = regress(log(edl_PAR(ipar(d1))),[ones(size(depth(impr(d1)))) depth(impr(d1))]);
+                    [K2,~,~,~,STATS2] = regress(log(edl_PAR(ipar(d2))),[ones(size(depth(impr(d2)))) depth(impr(d2))]);
+                    
+                    impr1=impr(d1);
+                    impr2=impr(d2);
+                    ipar1=ipar(d1);
+                    ipar2=ipar(d2);
+                    
+                    K_PAR(filenum).file=filename;
+                    K_PAR(filenum).stats1=STATS1;
+                    K_PAR(filenum).K1=K1;
+                    K_PAR(filenum).depth_index1=impr1;
+                    K_PAR(filenum).par_index1=ipar1;
+                    K_PAR(filenum).stats2=STATS2;
+                    K_PAR(filenum).K2=K2;
+                    K_PAR(filenum).depth_index2=impr2;
+                    K_PAR(filenum).par_index2=ipar2;
+                    K_PAR(filenum).NOTES=['split cast from 0-' num2str(dbreak(1)) ' and ' num2str(dbreak(2)) '-max'];
+                    K_PAR(filenum).flag=3;
+                    
+                    subplot(1,2,1,'replace')
+                    [ax h1 h2]=plotyy(tempdata(filenum).mprtime,depth,tempdata(filenum).mprtime(edl_ind),edl_PAR);
+                    hold(ax(1)); hold(ax(2));
+                    plot(ax(1),tempdata(filenum).mprtime(impr1),depth(impr1),'.','markersize',12,'color',[0.2081    0.1663    0.5292])
+                    plot(ax(2),tempdata(filenum).mprtime(impr1),edl_PAR(ipar1),'.','markersize',12,'color',[0.2081    0.1663    0.5292])
+                    plot(ax(1),tempdata(filenum).mprtime(impr2),depth(impr2),'.','markersize',12,'color',[0.9763    0.9831    0.0538])
+                    plot(ax(2),tempdata(filenum).mprtime(impr2),edl_PAR(ipar2),'.','markersize',12,'color',[0.9763    0.9831    0.0538])
+                    set(ax(1),'YDir','reverse','xgrid','on','ygrid','on')
+                    title(datafolders{foldernum})
+                    ylabel(ax(1),'Depth')
+                    ylabel(ax(2),'PAR')
+                    datetick(ax(2))
+                    datetick(ax(1))
+                    
+                    subplot(1,2,2,'replace')
+                    plot(log(edl_PAR),depth(edl_ind),'.'), hold on
+                    plot(log(edl_PAR(ipar1)),depth(impr1),'.','color',[0.2081    0.1663    0.5292])
+                    plot(K1(1)+K1(2)*depth(impr1),depth(impr1),'-')
+                    plot(log(edl_PAR(ipar2)),depth(impr2),'.','color',[0.9763    0.9831    0.0538])
+                    plot(K2(1)+K2(2)*depth(impr2),depth(impr2),'-')
+                    set(gca,'YDir','reverse')
+                    title(['Folder: ' num2str(foldernum) ', File: ' num2str(filenum) ' out of ' num2str(length(K_PAR)) '; ' filename ' SPLIT CAST!'])
+                    xl=get(gca,'xlim');  yl=get(gca,'ylim');
+                    text(0.80*diff(xl)+xl(1),0.85*diff(yl)+yl(1),{['K1: ' num2str(K1(2))];['R2: ' num2str(STATS1(1))] })
+                    text(0.80*diff(xl)+xl(1),0.95*diff(yl)+yl(1),{['K2: ' num2str(K2(2))];['R2: ' num2str(STATS2(1))] })
+                    
+                    keyboard
+                    
+                case 'other'
+                    
+                    keyboard
+                    
+                    %by trial and error to figure out which indexes to
+                    %inlcude:
+                    
+                    imprB=impr(15:end); %if want to exclude some suspicious data points
+                    iparB=ipar(15:end);
+                    
+                    [K,~,~,~,STATS] = regress(log(edl_PAR(iparB)),[ones(size(depth(imprB))) depth(imprB)]);
+                    
+                    subplot(1,2,2)
+                    plot(K(1)+K(2)*depth(imprB),depth(imprB),'-')
+                    text(0.80*diff(xl)+xl(1),0.95*diff(yl)+yl(1),{['K: ' num2str(K(2))];['R2: ' num2str(STATS(1))] })
+                    plot(log(edl_PAR),depth(edl_ind),'.'), hold on
+                    plot(log(edl_PAR(iparB)),depth(imprB),'.')
+            
+                    K_PAR(filenum).file=filename;
+                    K_PAR(filenum).stats=STATS;
+                    K_PAR(filenum).K=K;
+                    K_PAR(filenum).depth_index=imprB;
+                    K_PAR(filenum).par_index=iparB;
+                    K_PAR(filenum).NOTES='manually excluded points';
+                    K_PAR(filenum).flag=0;
+                    
+                    subplot(1,2,1,'replace')
+                    [ax h1 h2]=plotyy(tempdata(filenum).mprtime,depth,tempdata(filenum).mprtime(edl_ind),edl_PAR);
+                    hold(ax(1)); hold(ax(2));
+                    plot(ax(1),tempdata(filenum).mprtime(impr),depth(impr),'.','markersize',12)
+                    plot(ax(2),tempdata(filenum).mprtime(impr),edl_PAR(ipar),'.','markersize',12)
+                    set(ax(1),'YDir','reverse','xgrid','on','ygrid','on')
+                    ylabel(ax(1),'Depth')
+                    ylabel(ax(2),'PAR')
+                    datetick(ax(2))
+                    datetick(ax(1))
+                    
+                    plot(ax(1),tempdata(filenum).mprtime(imprB),depth(imprB),'.','markersize',12)                    
+                    plot(ax(2),tempdata(filenum).mprtime(imprB),edl_PAR(iparB),'.','markersize',12)
+                    %if need to exclude a cast:
+                    % casts=find(diff(impr)>100);
+                    % cc=7;
+                    % [K,~,~,~,STATS] = regress(log(edl_PAR(ipar(casts(cc):end))),[ones(size(depth(impr(casts(cc):end)))) depth(impr(casts(cc):end))]);
+                    
+                    keyboard
+                otherwise
+                    keyboard
+            end
+            
+            %             if STATS(1) < 0.9
+            %                 disp('Hmmm...maybe not such a great regression fit to find k?')
+            %             end
             
         end
-        
-        keyboard
-        
-        %if need to exclude a cast:
-        % casts=find(diff(impr)>100);
-        % cc=7;
-        % [K,~,~,~,STATS] = regress(log(edl_PAR(ipar(casts(cc):end))),[ones(size(depth(impr(casts(cc):end)))) depth(impr(casts(cc):end))]);
         
     end
     
@@ -194,7 +280,7 @@ clear tempdata mprtime edl_ind edl_PAR depth
 
 %save the attenuation fits:
 eval(['K_PAR_' datafolders{foldernum} '=K_PAR;'])
-eval(['save ' matsource 'K_PAR_' datafolders{foldernum} '.mat K_PAR_' datafolders{foldernum}])
+eval(['save ' matsource 'K_PAR_' datafolders{foldernum} 'B.mat K_PAR_' datafolders{foldernum}])
 
 %disp K's:
 k_rec=[];
@@ -284,16 +370,16 @@ for filenum=1:length(K_PAR);
         depth=tempdata(filenum).depth;
         edl_ind=tempdata(filenum).edl_ind;
         edl_PAR=tempdata(filenum).edl_PAR;
-
-        STATS1=K_PAR(filenum).stats;
+        
+        STATS1=K_PAR(filenum).stats1;
         K1=K_PAR(filenum).K1;
         impr1=K_PAR(filenum).depth_index1;
         ipar1=K_PAR(filenum).par_index1;
-        STATS2=K_PAR(filenum).stats;
+        STATS2=K_PAR(filenum).stats2;
         K2=K_PAR(filenum).K2;
         impr2=K_PAR(filenum).depth_index2;
         ipar2=K_PAR(filenum).par_index2;
-
+        
         subplot(1,2,1,'replace')
         [ax h1 h2]=plotyy(tempdata(filenum).mprtime,depth,tempdata(filenum).mprtime(edl_ind),edl_PAR);
         hold(ax(1)); hold(ax(2));
